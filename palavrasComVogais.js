62a28272127fba0711eb28e8bb95fe7e02d84775
Ana Carolina <126017853+carolrolis@users.noreply.github.com>
2025-02-20 20:46:45 -0300

    Create palavrasComVogais.js

---
+++ palavrasComVogais.js
@@ -0,0 +1,28 @@
+const str = "roxo amarelo laranja verde violeta azul indigo";
+
+
+function inicioVogal(string) {
+  const palavras = string.split(" ");
+  const vogais = "aeiou";
+  let palavrasInicioVogal = [];
+  let palavrasQuantidade = 0;
+
+
+  for (let vogal of vogais) {
+    for (let palavra of palavras) {
+      if (palavra[0] === vogal) {
+        palavrasInicioVogal.push(palavra);
+        palavrasQuantidade++;
+      }
+    }
+  }
+
+
+  console.log(
+    "Palavras que iniciam com uma vogal: " + palavrasInicioVogal.join(", ")
+  );
+  console.log("Total: " + palavrasQuantidade);
+}
+
+
+inicioVogal(str);
